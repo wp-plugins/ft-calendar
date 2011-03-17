99dc684b046e888e10a04ed10fd38b916b701345
blepoxp <blepoxp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-17 18:40:52 +0000

    Barebones. Pre intial-checkin
    git-svn-id: https://plugins.svn.wordpress.org/ft-calendar/tags/0.2@361350 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ft-calendar.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package FT_Calendar
+ * @version 0.2/
+/*
+Plugin Name: FullThrottle Calendar
+Plugin URI: http://fullthrottlecalendar.com/ft-calendar
+Description: A feature rich calendar plugin for WordPress.
+Author: FullThrottle Development
+Version: 0.2
+Author URI: http://fullthrottledevelopment.com/
+Primary Developer: Glenn Ansley (glenn@glennansley.com)
+Primary Developer: Lew Ayotte (lew@lewayotte.com)
+*/
+
+#### CONSTANTS ####
+	define( 'FT_CAL_VERSION', '0.2' );
+	define( 'FT_CAL_DB_VERSION', '0.2' );
+	
+// FT Calendar will be released very shortly.
